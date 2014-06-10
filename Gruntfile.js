b261872a1a64249c5c74a53ff34d20cae2b10554
mirazmamun <mirazmamun@yahoo.com>
2014-06-10 14:04:18 +1000

    Updated the Gruntfile to get rid of unnecessaries

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,27 +2,6 @@ module.exports = function(grunt){
 	//the initial configuration
 	grunt.initConfig({
 		package: grunt.file.readJSON('package.json'),
-		less:{
-			compile:{
-				files:{
-					'css/*.css':'less/*.less'
-				}
-			}
-		},
-		coffee:{
-			compile:{
-				files:{
-					'js/site.js':['coffee/*.coffee']
-				}
-			}
-		},
-		watch:{
-			files:['less/*.less', 'index.html'],
-			tasks:['less', 'coffee'],
-			options:{
-				livereload: true
-			}
-		},
 		express:{
 			all:{
 				options:{
@@ -38,13 +17,6 @@ module.exports = function(grunt){
 		    files: ['js/site.js']
 		}
 	});
-	//the loadtask
-	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-contrib-coffee');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-express');
-	grunt.registerTask('default',['less', 'coffee']);
-	grunt.registerTask('server',['express','watch']);
 	
 	//the travis task
   	grunt.registerTask('travis', 'lint');
